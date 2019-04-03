80d11b12149360e90b38ca604c5f28237ee93242
Roopak R Tamboli <rrtamboli@gmail.com>
2019-04-03 22:31:35 +0530

    res_created

--- compile.bat
+++ compile.bat
@@ -2,3 +2,4 @@ python jemdoc.py index.jemdoc
 python jemdoc.py exp.jemdoc
 python jemdoc.py edu.jemdoc
 python jemdoc.py pub.jemdoc
+python jemdoc.py res.jemdoc
--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python jemdoc.py index.jemdoc && python jemdoc.py exp.jemdoc && python jemdoc.py edu.jemdoc && python jemdoc.py pub.jemdoc
+python jemdoc.py index.jemdoc && python jemdoc.py exp.jemdoc && python jemdoc.py edu.jemdoc && python jemdoc.py pub.jemdoc && python jemdoc.py res.jemdoc
