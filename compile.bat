3bb6d1baf49da7d3b0dfb60ed2df1e05d0397fbd
Roopak R Tamboli <rrtamboli@gmail.com>
2021-07-04 15:48:09 +0200

    split publications

--- compile.bat
+++ compile.bat
@@ -1,5 +1,7 @@
 python jemdoc.py index.jemdoc
 python jemdoc.py exp.jemdoc
 python jemdoc.py edu.jemdoc
-python jemdoc.py pub.jemdoc
+python jemdoc.py overview.jemdoc
+python jemdoc.py jrnl.jemdoc
+python jemdoc.py conf.jemdoc
 python jemdoc.py res.jemdoc
