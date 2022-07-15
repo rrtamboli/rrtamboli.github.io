3cb7c1e4433bf807a0225b055a0cb7f17473943f
rrtamboli <rrtamboli@gmail.com>
2022-07-15 20:27:11 +0530

    python 2 shell file

---
+++ compile_py2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python2 jemdoc.py index.jemdoc && python2 jemdoc.py exp.jemdoc && python2 jemdoc.py edu.jemdoc && python2 jemdoc.py overview.jemdoc && python2 jemdoc.py res.jemdoc && python2 jemdoc.py conf.jemdoc && python2 jemdoc.py jrnl.jemdoc && python2 jemdoc.py outreach.jemdoc && python2 jemdoc.py service.jemdoc
